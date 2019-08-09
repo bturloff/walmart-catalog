c3bf4951630ed2efd988293c9cacdd9b73d48425
Brian Turloff <brian.turloff@gmail.com>
2019-08-09 01:42:54 -0700

    Add navbar, search input field, product card placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { searchField: "" };
+
+  _handleKeyDown = e => {
+    if (e.key === "Enter") {
+      console.log("do validate");
+    }
+  };
+
+  _handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="input-field">
+            <input
+              id="search"
+              type="text"
+              className="validate"
+              value={this.state.searchField}
+              onChange={this._handleChange}
+              onKeyDown={this._handleKeyDown}
+            />
+            <label htmlFor="search">Search</label>
+          </div>
+
+          <div className="row">
+            <div className="col s12 m6">
+              <div className="card blue-grey darken-1">
+                <div className="card-content white-text">
+                  <span className="card-title">Card Title</span>
+                  <p>
+                    I am a very simple card. I am good at containing small bits
+                    of information. I am convenient because I require little
+                    markup to use effectively.
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#">This is a link</a>
+                  <a href="#">This is a link</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="*" className="brand-logo">
+          <img src="walmartlogo.png" alt="" />
+        </a>
+
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="*">
+              <i className="large material-icons">account_circle</i>
+            </a>
+          </li>
+          <li>
+            <a href="*">
+              <i className="large material-icons">location_on</i>
+            </a>
+          </li>
+          <li>
+            <a href="*">
+              <i className="large material-icons">shopping_cart</i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
